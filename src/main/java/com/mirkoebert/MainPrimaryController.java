38e987bea41abb1194259fe6f93cf7cfa7139b34
Mirko Ebert <mirko@ebert-p.com>
2026-05-30 22:36:10 +0200

    refactor: replace principal by user

--- src/main/java/com/mirkoebert/MainPrimaryController.java
+++ src/main/java/com/mirkoebert/MainPrimaryController.java
@@ -4,10 +4,10 @@ import com.mirkoebert.advisor.AdvisorService;
 import com.mirkoebert.handicap.HcpService;
 import com.mirkoebert.sgi.SgiHcpAggregatedService;
 import com.mirkoebert.timeline.TimelineService;
+import com.mirkoebert.user.CurrentUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import lombok.val;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,31 +23,33 @@ public class MainPrimaryController {
         private final HcpService hcpService;
         private final SgiHcpAggregatedService sgiHcpAggregatedService;
         private final AdvisorService advisorService;
+        private final CurrentUserService currentUserService;
 
         @GetMapping("/user-page")
-        public String getUser(Model model, @AuthenticationPrincipal OAuth2User principal) {
-                final String u = (String) principal.getAttributes().get("sub");
+        public String getUser(Model model) {
+                val u = currentUserService.getCurrentUser();
                 log.info("user page {}", u);
-                model.addAttribute("name", principal.getAttributes().get("name"));
-                model.addAttribute("email", principal.getAttributes().get("email"));
-                model.addAttribute("lastHCP", hcpService.findLatestByUserId(u).getHcp());
-                model.addAttribute("lastSGHCP", sgiHcpAggregatedService.getLatestSgiHcpAggregated(u));
-                model.addAttribute("advice", advisorService.getAdvise(u));
-                String fullUrl = (String) principal.getAttributes().get("picture");
+                model.addAttribute("name", u.name());
+                model.addAttribute("email", u.email());
+                model.addAttribute("lastHCP", hcpService.findLatestByUserId(u.id()).getHcp());
+                model.addAttribute("lastSGHCP", sgiHcpAggregatedService.getLatestSgiHcpAggregated(u.id()));
+                model.addAttribute("advice", advisorService.getAdvise(u.id()));
+                String fullUrl = u.pictureUrl();
                 String pureUrl = fullUrl.substring(0, fullUrl.lastIndexOf("="));
                 model.addAttribute("picture", pureUrl);
                 return "user";
         }
 
         @GetMapping("/timeline")
-        public String getTimeline(Model m, @AuthenticationPrincipal OAuth2User principal) {
-                log.info("timeline page {}", principal.getAttributes());
-                m.addAttribute("timeline", timeService.getLatestResults((String) principal.getAttributes().get("sub")));
+        public String getTimeline(Model m) {
+                log.info("timeline page");
+                val u = currentUserService.getCurrentUser();
+                m.addAttribute("timeline", timeService.getLatestResults(u.id()));
                 return "timeline";
         }
 
         @GetMapping("/about")
-        public String getAbout(final Model m, @AuthenticationPrincipal final OAuth2User principal) {
+        public String getAbout(final Model m) {
                 log.info("about page");
                 m.addAttribute("version", this.getClass().getPackage().getImplementationVersion());
                 return "about";
--- src/main/java/com/mirkoebert/sgi/chart/SgiPrimaryRestController.java
+++ src/main/java/com/mirkoebert/sgi/chart/SgiPrimaryRestController.java
@@ -1,10 +1,10 @@
 package com.mirkoebert.sgi.chart;
 
+import com.mirkoebert.user.CurrentUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,14 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class SgiPrimaryRestController {
 
         private final MonthlySgiHcpAggregator monthlySgiHcpAggregator;
+        private final CurrentUserService currentUserService;
 
         @GetMapping("/api/sgi/chart-data/{testId}")
-        public ResponseEntity<HcpData> getLineChartData(@AuthenticationPrincipal OAuth2User principal, @PathVariable String testId) {
+        public ResponseEntity<HcpData> getLineChartData(@PathVariable String testId) {
                 log.info("getLineChartData testId {}", testId);
-                final String userId = (String) principal.getAttributes().get("sub");
+                val u = currentUserService.getCurrentUser();
+                final String userId = u.id();
                 log.info("for user {}", userId);
                 return ResponseEntity.ok(monthlySgiHcpAggregator.getHcpForLastMonth(12, userId, Integer.parseInt(testId)));
         }
 
 }
-
