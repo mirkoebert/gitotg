4c6ca9cc815e3723234bd05c70ec8b43729b84e7
Mirko Ebert <mirko@ebert-p.com>
2026-06-04 18:35:00 +0200

    feature: more advices

--- src/main/java/com/mirkoebert/advisor/AdvisorService.java
+++ src/main/java/com/mirkoebert/advisor/AdvisorService.java
@@ -27,7 +27,7 @@ public class AdvisorService {
                 "Let's produce some data.",
                 "Data is what I need. Start a test.",
                 "Data is what I need. Go to the chipping green and start a training.",
-                "Monitoring your results is a cornerstone of improvement."
+                "Monitoring your results is a cornerstone of improvement.",
         };
 
         final static String[] few = {
@@ -38,14 +38,39 @@ public class AdvisorService {
         };
 
 
+        final static String[] beginner = {
+                "Go and train.",
+                "Play tournaments. Don't be shy. Every Golfer started at 54.",
+                "Buy only cheap balls"
+        };
+
         final static String[] hh = {
                 "High Handicaper lower sores faster by improving the short game.",
-                "Short game is a cornerstone of improvement for High Handicapers."
+                "Short game is a cornerstone of improvement for High Handicapers.",
+                "Your clubs are not the problem.",
+                "Play your shot shape.",
+                "Don't waste your time train shots you never hit. Go with your stock shots.",
+                "Every training session should have a specific goal.",
+                "Don't look at any YouTube Golf swing chage video."
+        };
+
+        final static String[] mh = {
+                "Think about club fitting. It could improve your game.",
+                "Consistend ball striking is king.",
+                "Improve aiming at long shots.",
+                "Walk before you run. On the range, start with ball contact shots before go ahead.",
+                "Improve your athletics."
         };
 
         final static String[] other ={
                 "I have to think about it. Go on.",
-                "Nice data. Keep up on golfing."
+                "Nice data. Keep up on golfing.",
+                "The Putter is never the problem.",
+                "Golf is a fun sport.",
+                "Golf is the Greatest Game",
+                "Golf jokes are the whorst kind of jokes.",
+                "Golf is addictive.",
+                "Alice Cooper is playing Golf."
         };
 
         private final Random r = new Random();
