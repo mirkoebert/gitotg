7d3b58072cfcf8a3f1d8f198730f3ddf38e58bd3
Mirko Ebert <mirko@ebert-p.com>
2026-07-21 14:55:37 +0200

    improve check lists

--- src/main/java/com/mirkoebert/checklist/CheckListGenerator.java
+++ src/main/java/com/mirkoebert/checklist/CheckListGenerator.java
@@ -18,27 +18,31 @@ public class CheckListGenerator {
         
         @PostConstruct
         void init() {
-                if (repo.count() > 0) {
-                        log.debug("There is data in the db. Skip loading initial data set");
-                        return;
-                }
                 List<GolfCheckListItem> l = new ArrayList<>();
                 l.add(new GolfCheckListItem(null, "Cheap Balls", "Play cheap golf balls, one buck per ball is Ok at this level. You don't benefit from expensive balls. You are losing too many balls", GoalEnum.BREAK100.name()));
-                l.add(new GolfCheckListItem(null, "Grip", "Before you invest time in other things like your swing check and learn the right grip.", GoalEnum.BREAK100.name()));
+                l.add(new GolfCheckListItem(null, "Improve Grip", "Before you invest time in other things like your swing, check and learn the right grip.", GoalEnum.BREAK100.name()));
                 l.add(new GolfCheckListItem(null, "Win a tournament (netto)", "On a good day you are capable to win a club tournament.", GoalEnum.BREAK100.name()));
-                l.add(new GolfCheckListItem(null, "Without driver", "Master your irons. Play without driver or 3 wood. You can break 100 without driver.", GoalEnum.BREAK100.name()));
+                l.add(new GolfCheckListItem(null, "Play without driver", "Master your irons. Play without driver or 3 wood. You can break 100 without driver.", GoalEnum.BREAK100.name()));
                 l.add(new GolfCheckListItem(null, "Consistent 150 yard / 145 m shot", "Carry 150 yards is all you need. Whatever it is, it's enough to break 100.", GoalEnum.BREAK100.name()));
-                l.add(new GolfCheckListItem(null, "Improve your short game", "Start with short game HCP.", GoalEnum.BREAK100.name()));
+                l.add(new GolfCheckListItem(null, "Improve your short game", "Short game improvements are the fastes way to lower your handicapStart with short game HCP.", GoalEnum.BREAK100.name()));
                 l.add(new GolfCheckListItem(null, "Short putts", "1 yard / 1m putt with consistency > 90% rate, every time", GoalEnum.BREAK100.name()));
-                l.add(new GolfCheckListItem(null, "Warm up", "Learn a good warmup routine to get juice in the joints, to loosen your muscles.", GoalEnum.BREAK100.name()));
+                l.add(new GolfCheckListItem(null, "Warm up before playing", "Learn a good warmup routine to get juice in the joints, to loosen your muscles.", GoalEnum.BREAK100.name()));
                 l.add(new GolfCheckListItem(null, "Range first", "One bucket of range balls is cheaper than losing two balls on the course", GoalEnum.BREAK100.name()));
                 l.add(new GolfCheckListItem(null, "Know your distances", "Get the carry distances for clubs from the tee.", GoalEnum.BREAK100.name()));
-
+                l.add(new GolfCheckListItem(null, "Learn to calc the distances", "Learn to estimate the distances for your shot.", GoalEnum.BREAK100.name()));
 
                 l.add(new GolfCheckListItem(null, "Consistent 160 yard / 150 m shot", "Carry. Whatever it is, it's enough to break 90.", GoalEnum.BREAK90.name()));
                 l.add(new GolfCheckListItem(null, "Improve your short game", "Go ahead with short game HCP.", GoalEnum.BREAK90.name()));
-                l.add(new GolfCheckListItem(null, "Consistency is king", "Start with consistency games.", GoalEnum.BREAK90.name()));
+                l.add(new GolfCheckListItem(null, "Consistency is king", "Start with consistency games on the range and on the greens.", GoalEnum.BREAK90.name()));
                 l.add(new GolfCheckListItem(null, "Preshot routine", "Preshot routine, every time, I said, every time. Start with a simple one.", GoalEnum.BREAK90.name()));
+                l.add(new GolfCheckListItem(null, "Collect game statistics", "Identify your weaknesses", GoalEnum.BREAK90.name()));
+
+
+                if (repo.count() == l.size()) {
+                        log.debug("There is data in the db. Skip loading initial data set");
+                        return;
+                }
+                repo.deleteAll();
                 repo.saveAll(l);
         }
 }
