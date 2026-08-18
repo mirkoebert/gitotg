1963d6fd2d554fc21d03079055a749abede25a7d
Mirko Ebert <mirko@ebert-p.com>
2026-08-18 12:36:33 +0200

    feature: add another tip

--- src/main/java/com/mirkoebert/advisor/AdvisorService.java
+++ src/main/java/com/mirkoebert/advisor/AdvisorService.java
@@ -64,7 +64,8 @@ public class AdvisorService {
             "advisor.other.7",
             "advisor.other.8",
             "advisor.other.9",
-            "advisor.other.10"
+            "advisor.other.10",
+            "advisor.other.11"
     };
     private final HcpRepository hcpRepository;
     private final SingleTestResultRepository singleTestResultRepository;
